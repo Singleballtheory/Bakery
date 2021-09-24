287a6eee44db72ecf02459d0c6a3830bfadd958f
Scott Hutley <scotthutley1@comcast.net>
2021-09-24 15:42:42 -0700

    Add Console greeting and simple exit

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -8,7 +8,17 @@ namespace Bakery
   {
     public static void Main()
     {
+      Console.Clear();
 
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("We've got bread loaves for $5 each, or Buy Two Get 1 Free!");
+      Console.WriteLine("We also have fresh pastries for $2 each, or Three for $5!");
+      Console.WriteLine("Care to place an order? (yes/no)");
+
+      string takeOrder = Console.ReadLine();
+      if (takeOrder == "no" || takeOrder == "No") {
+        Console.WriteLine("Take care then and thanks for coming in!");
+      }
 
     }
   }
