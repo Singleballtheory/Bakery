60a6dde605637b196a320c6a98f12ab547bed57d
Scott Hutley <scotthutley1@comcast.net>
2021-09-24 16:54:35 -0700

    WIP Down the Console.WriteLine rabbit hole

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -15,9 +15,26 @@ namespace Bakery
       Console.WriteLine("We also have fresh pastries for $2 each, or Three for $5!");
       Console.WriteLine("Care to place an order? (yes/no)");
 
-      string takeOrder = Console.ReadLine();
-      if (takeOrder == "no" || takeOrder == "No") {
+      string breadStart = Console.ReadLine();
+      if (breadStart.ToLower() == "no") {
         Console.WriteLine("Take care then and thanks for coming in!");
+      } else if (breadStart.ToLower() == "yes") {
+        Console.WriteLine("Fantastic! Let's start with some bread!");
+        Console.WriteLine("How many loaves would you like: ");
+        string breadOrder = Console.ReadLine();
+        int breadAmount = int.Parse(breadOrder);
+        Bread freshBread = new Bread(breadAmount);
+        freshBread.SetPrice();
+        Console.WriteLine($"An order of {breadAmount} will be ${freshBread.Price}!");
+        Console.WriteLine("Discounts already applied! Check the math!");
+        Console.WriteLine("Now how about some pastries? (yes/no)");
+        // Stopped here for the moment
+        if (breadAmount == 0) {
+          Console.WriteLine("No bread today? How about some pastries then? (yes/no)");
+          // string pastryOrder = Console.ReadLine();
+          // if (pastryOrder);
+
+        }
       }
 
     }
