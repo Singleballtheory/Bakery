e8e876599e4f708a337654b05316e980424d04a5
Scott Hutley <scotthutley1@comcast.net>
2021-09-24 09:56:07 -0700

    Create Bread class with test that calls to Bread

--- Bakery.Tests/ModelTests/OffersTests.cs
+++ Bakery.Tests/ModelTests/OffersTests.cs
@@ -2,8 +2,18 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using Bakery;
+using Offers.Models;
 
 namespace Bakery.Tests
 {
-
+  [TestClass]
+  public class OffersTests
+  {
+    [TestMethod]
+    public void BreadOrder_OrderOneBread_Bread()
+    {
+      Bread freshBread = new Bread(1);
+      Assert.AreEqual(typeof(Bread), freshBread.GetType());
+    }
+  }
 }
--- Bakery/Models/Offers.cs
+++ Bakery/Models/Offers.cs
@@ -5,6 +5,12 @@ namespace Offers.Models
 {
   public class Bread
   {
-   
+    public int Amount { get; set; }
+    public int Price { get; set; }
+    public Bread(int amount, int price = 0)
+    {
+      Amount = amount;
+      Price = price;
+    }
   }
 }
